f9a14c56b71bf28761572339965dc8b126f7cb24
João Lucas Barboza <joaolucasbarbozaa@gmail.com>
2026-09-09 01:53:27 -0300

    Revert "initial commit"
    This reverts commit 6287091d9360010106ac24e88bf0b0235e2a0c03.

---
+++ android/app/src/main/kotlin/com/joaobarboza/play_trophy/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.joaobarboza.play_trophy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/ui/home/widgets/home_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  final String name = "João";
+  final String plataform = "PlayStation5";
+
+  const HomeScreen({super.key});
+
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentPageIndex = 0;
+  final Duration durationMs = Duration(milliseconds: 500);
+
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        selectedIndex: currentPageIndex,
+        animationDuration: durationMs,
+        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+        destinations: [
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home_rounded),
+            icon: Icon(Icons.home_outlined),
+            label: "Início",
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.collections_bookmark),
+            icon: Icon(Icons.collections_bookmark_outlined),
+            label: "Biblioteca",
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.person_rounded),
+            icon: Icon(Icons.person_outline),
+            label: "Perfil",
+          ),
+        ],
+      ),
+    );
+  }
+}
